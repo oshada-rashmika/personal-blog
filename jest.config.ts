78c5044f153e40d3322db91b240673ea40ecc302
Oshada Rashmika <oshadar.rodrigo@gmail.com>
2026-05-17 13:03:40 +0530

    test: integrate Jest and React Testing Library environments

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import nextJest from 'next/jest.js';
+import type { Config } from 'jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  coverageProvider: 'v8',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+export default createJestConfig(config);
